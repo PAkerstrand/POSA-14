53156677db7482d55a40b35cd749266de7778353
PAkerstrand <patrik.akerstrand@aftonbladet.se>
2014-06-10 20:20:37 +0200

    Implemented assignment 4

--- assignments/week-4-assignment-3/src/edu/vuum/mocca/PingPongRight.java
+++ assignments/week-4-assignment-3/src/edu/vuum/mocca/PingPongRight.java
@@ -50,6 +50,7 @@ public class PingPongRight {
          * iteration.
          */
         // TODO - You fill in here.
+        private final String mStringToPrint;
 
         /**
          * Two SimpleSemaphores use to alternate pings and pongs.  You
@@ -57,6 +58,7 @@ public class PingPongRight {
          * two data members.
          */
         // TODO - You fill in here.
+        private final SimpleSemaphore[] mSemaphores;
 
         /**
          * Constructor initializes the data member(s).
@@ -66,6 +68,9 @@ public class PingPongRight {
                                   SimpleSemaphore semaphoreTwo,
                                   int maxIterations) {
             // TODO - You fill in here.
+            mStringToPrint = stringToPrint;
+            mSemaphores = new SimpleSemaphore[]{semaphoreOne, semaphoreTwo};
+            mMaxLoopIterations = maxIterations;
         }
 
         /**
@@ -80,6 +85,16 @@ public class PingPongRight {
              */
 
             // TODO - You fill in here.
+            for(int currLoop = 1; currLoop <= mMaxLoopIterations; ++currLoop) {
+                acquire();
+                printOutput(currLoop);
+                release();
+            }
+            mLatch.countDown();
+        }
+
+        private void printOutput(int iteration) {
+            System.out.println(mStringToPrint + "(" + iteration + ")");
         }
 
         /**
@@ -87,6 +102,7 @@ public class PingPongRight {
          */
         private void acquire() {
             // TODO fill in here
+            mSemaphores[FIRST_SEMA].acquireUninterruptibly();
         }
 
         /**
@@ -94,6 +110,7 @@ public class PingPongRight {
          */
         private void release() {
             // TODO fill in here
+            mSemaphores[SECOND_SEMA].release();
         }
     }
 
@@ -108,36 +125,32 @@ public class PingPongRight {
 
         // TODO initialize this by replacing null with the appropriate
         // constructor call.
-        mLatch = null;
+        mLatch = new CountDownLatch(2);
 
         // Create the ping and pong SimpleSemaphores that control
         // alternation between threads.
 
         // TODO - You fill in here, make pingSema start out unlocked.
-        SimpleSemaphore pingSema = null;
+        SimpleSemaphore pingSema = new SimpleSemaphore(1, false);
         // TODO - You fill in here, make pongSema start out locked.
-        SimpleSemaphore pongSema = null;
+        SimpleSemaphore pongSema = new SimpleSemaphore(0, false);
 
         System.out.println(startString);
 
-        // Create the ping and pong threads, passing in the string to
+        // TODO - Create the ping and pong threads, passing in the string to
         // print and the appropriate SimpleSemaphores.
-        PlayPingPongThread ping = new PlayPingPongThread(/*
-                                                          * TODO - You fill in
-                                                          * here
-                                                          */);
-        PlayPingPongThread pong = new PlayPingPongThread(/*
-                                                          * TODO - You fill in
-                                                          * here
-                                                          */);
+        PlayPingPongThread ping = new PlayPingPongThread(pingString, pingSema, pongSema, maxIterations);
+        PlayPingPongThread pong = new PlayPingPongThread(pongString, pongSema, pingSema, maxIterations);
 
         // TODO - Initiate the ping and pong threads, which will call
         // the run() hook method.
+        ping.start();
+        pong.start();
 
         // TODO - replace the following line with a barrier
         // synchronizer call to mLatch that waits for both threads to
         // finish.
-        throw new java.lang.InterruptedException();
+        mLatch.await();
 
         System.out.println(finishString);
     }
--- assignments/week-4-assignment-3/src/edu/vuum/mocca/SimpleSemaphore.java
+++ assignments/week-4-assignment-3/src/edu/vuum/mocca/SimpleSemaphore.java
@@ -1,36 +1,45 @@
 package edu.vuum.mocca;
+
 import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * @class SimpleSemaphore
  * 
- * @brief This class provides a simple counting semaphore implementation using
- *        Java a ReentrantLock and a ConditionObject (which is accessed via a
- *        Condition). It must implement both "Fair" and "NonFair" semaphore
- *        semantics, just liked Java Semaphores.
+ * @brief This class provides a simple counting semaphore
+ *        implementation using Java a ReentrantLock and a
+ *        ConditionObject (which is accessed via a Condition). It must
+ *        implement both "Fair" and "NonFair" semaphore semantics,
+ *        just liked Java Semaphores.
  */
 public class SimpleSemaphore {
     /**
      * Define a ReentrantLock to protect the critical section.
      */
     // TODO - you fill in here
+    final Lock mLock;
 
     /**
      * Define a Condition that waits while the number of permits is 0.
      */
     // TODO - you fill in here
+    final Condition mPermitAvailable;
 
     /**
      * Define a count of the number of available permits.
      */
-    // TODO - you fill in here. Make sure that this data member will
+    // TODO - you fill in here.  Make sure that this data member will
     // ensure its values aren't cached by multiple Threads..
+    int mPermits;
 
     public SimpleSemaphore(int permits, boolean fair) {
         // TODO - you fill in here to initialize the SimpleSemaphore,
         // making sure to allow both fair and non-fair Semaphore
         // semantics.
+        mPermits = permits;
+        mLock = new ReentrantLock(fair);
+        mPermitAvailable = mLock.newCondition();
     }
 
     /**
@@ -39,6 +48,15 @@ public class SimpleSemaphore {
      */
     public void acquire() throws InterruptedException {
         // TODO - you fill in here.
+        mLock.lockInterruptibly();
+        try {
+            while (mPermits == 0) {
+                mPermitAvailable.await();
+            }
+            --mPermits;
+        } finally {
+            mLock.unlock();
+        }
     }
 
     /**
@@ -47,6 +65,16 @@ public class SimpleSemaphore {
      */
     public void acquireUninterruptibly() {
         // TODO - you fill in here.
+        mLock.lock();
+        try {
+            while (mPermits == 0) {
+                mPermitAvailable.await();
+            }
+            --mPermits;
+        } catch (InterruptedException e) {
+        } finally {
+            mLock.unlock();
+        }
     }
 
     /**
@@ -54,13 +82,21 @@ public class SimpleSemaphore {
      */
     void release() {
         // TODO - you fill in here.
+        mLock.lock();
+        try {
+            ++mPermits;
+            mPermitAvailable.signal();
+        } finally {
+            mLock.unlock();
+        }
     }
 
     /**
      * Return the number of permits available.
      */
     public int availablePermits() {
-        // TODO - you fill in here to return the correct result
-    	return 0;
+        // TODO - you fill in here by changing null to the appropriate
+        // return value.
+        return mPermits;
     }
 }
